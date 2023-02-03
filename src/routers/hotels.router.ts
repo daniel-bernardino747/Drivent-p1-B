a0a54de1cb7ffac603911df4cbb8805d05e75266
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-02-02 23:00:46 -0300

    feat: add controller in route GET  /hotels/:id

--- src/routers/hotels.router.ts
+++ src/routers/hotels.router.ts
@@ -1,9 +1,9 @@
-import { getHotels } from "@/controllers";
+import { getHotelById, getHotels } from "@/controllers";
 import { authenticateToken } from "@/middlewares";
 import { Router } from "express";
 
 const hotelsRouter = Router();
 
-hotelsRouter.all("/*", authenticateToken).get("/", getHotels).get("/:hotelId");
+hotelsRouter.all("/*", authenticateToken).get("/", getHotels).get("/:hotelId", getHotelById);
 
 export { hotelsRouter };
