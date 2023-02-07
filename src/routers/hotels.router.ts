9939d35e2fcf59a205fd0056a743820a0105b10c
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-02-06 22:01:39 -0300

    feat: remove wrong files

--- src/routers/hotels.router.ts
+++
@@ -1,9 +0,0 @@
-import { getHotelById, getHotels } from "@/controllers";
-import { authenticateToken } from "@/middlewares";
-import { Router } from "express";
-
-const hotelsRouter = Router();
-
-hotelsRouter.all("/*", authenticateToken).get("/", getHotels).get("/:hotelId", getHotelById);
-
-export { hotelsRouter };
